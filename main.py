4ac832cbe40b688e5633022ceab7f1e14ce33d5f
Jianne Roos <74767359+jianneroos@users.noreply.github.com>
2022-09-28 08:01:20 +0000

    zelf tegen een makkelijke tegenstander

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
-from bke import start
- 
-start()
+import random
+from bke import EvaluationAgent, start
+
+class MyRandomAgent(EvaluationAgent):
+    def evaluate(self, board, my_symbol, opponent_symbol):
+        return random.randint(1, 500)
+
+my_random_agent = MyRandomAgent()
+start(player_o=my_random_agent)
