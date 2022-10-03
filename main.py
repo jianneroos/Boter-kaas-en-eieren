9b7576abf6caedc7303fac8a76263d268a165bed
Jianne Roos <74767359+jianneroos@users.noreply.github.com>
2022-10-03 13:42:16 +0000

    zelf spelen tegen MLAgent

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
-import random
-from bke import EvaluationAgent, start
-
-class MyRandomAgent(EvaluationAgent):
-    def evaluate(self, board, my_symbol, opponent_symbol):
-        return random.randint(1, 500)
-
-my_random_agent = MyRandomAgent()
-start(player_x=my_random_agent, player_o=my_random_agent)
+from bke import MLAgent, is_winner, opponent, start
+ 
+ 
+class MyAgent(MLAgent):
+    def evaluate(self, board):
+        if is_winner(board, self.symbol):
+            reward = 1
+        elif is_winner(board, opponent[self.symbol]):
+            reward = -1
+        else:
+            reward = 0
+        return reward
+    
+ 
+my_agent = MyAgent()
+start(player_x=my_agent)
