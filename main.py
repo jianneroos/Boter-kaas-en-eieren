ee8a2f0c94e0a731219754788b4f8fa0a3bf7dff
Jianne Roos <74767359+jianneroos@users.noreply.github.com>
2022-10-05 08:03:31 +0000

    Menu deel 2

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import random
  
-from bke import start, EvaluationAgent, MLAgent, is_winner, opponent, train, save, RandomAgent, train_and_plot
- 
- 
+from bke import start, EvaluationAgent, MLAgent, is_winner, opponent, train, save, RandomAgent, load, train_and_plot
+
+class MyRandomAgent(EvaluationAgent):
+    def evaluate(self, board, my_symbol, opponent_symbol):
+        return random.randint(1, 500)
+
 class MyAgent(MLAgent):
     def evaluate(self, board):
         if is_winner(board, self.symbol):
@@ -12,57 +15,46 @@ class MyAgent(MLAgent):
         else:
             reward = 0
         return reward
-    
-    
-random.seed(1)
- 
-my_agent = MyAgent(alpha=1.0, epsilon=0.1)
-random_agent = RandomAgent()
 
 def anderpersoon():
   start()
 
-def dom()
-  class MyRandomAgent(EvaluationAgent):
-    def evaluate(self, board, my_symbol, opponent_symbol):
-        return random.randint(1, 500)
-
+def domtegen():
   my_random_agent = MyRandomAgent()
   start(player_o=my_random_agent)
 
-def trainen()
-	class MyAgent(MLAgent):
-    def evaluate(self, board):
-        if is_winner(board, self.symbol):
-            reward = 1
-        elif is_winner(board, opponent[self.symbol]):
-            reward = -1
-        else:
-            reward = 0
-        return reward
-    
- 
+def trainen():    
   my_agent = MyAgent()
  
   train(my_agent, 3000)
  
   save(my_agent, 'MyAgent_3000')
 
-def slim()
+def slimtegen():
+  my_agent = load('MyAgent_3000')
+ 
+  my_agent.learning = False
+ 
+  start(player_x=my_agent)
 
-
-def train_and_plot(
-        agent=my_agent,
-        validation_agent=random_agent,
-        iterations=50,
-        trainings=100,
-        validations=1000)
+def plot():
+  random.seed(1)
+ 
+  my_agent = MyAgent(alpha=0.8, epsilon=0.2)
+  #
+  random_agent = RandomAgent()
+  train_and_plot(
+      agent=my_agent,
+      validation_agent=random_agent,
+      iterations=50,
+      trainings=100,
+      validations=1000)
 
 print("1: Tegen een ander persoon spelen")
-print("1: Tegen een domme tegenstander spelen")
-print("1: De tegenstander trainen")
-print("1: Tegen een slimme tegenstander spelen")
-print("1: Je programma kan de validatie grafiek plotten
+print("2: Tegen een domme tegenstander spelen")
+print("3: De tegenstander trainen")
+print("4: Tegen een slimme tegenstander spelen")
+print("5: Je programma kan de validatie grafiek plotten")
 
 i = input()
 
@@ -70,13 +62,13 @@ if i == "1":
   anderpersoon()
 
 if i == "2":
-  dom()
+  domtegen()
 
 if i == "3":
   trainen()
 
 if i == "4":
-  slim()
+  slimtegen()
 
 if i == "5":
   plot()
